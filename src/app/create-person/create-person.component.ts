57a7da512b966a8a7aea92e82c821c3c0d5db38c
timburstefan <timbur.stefan14@gmail.com>
2021-09-07 21:37:15 +0300

    another changes

--- src/app/create-person/create-person.component.ts
+++ src/app/create-person/create-person.component.ts
@@ -4,7 +4,6 @@ import {
   EventEmitter,
   Output,
   ViewChild,
-  ElementRef,
 } from '@angular/core';
 
 @Component({
@@ -12,25 +11,20 @@ import {
   templateUrl: './create-person.component.html',
   styleUrls: ['./create-person.component.css'],
 })
-export class CreatePersonComponent implements OnInit {
+export class CreatePersonComponent {
   @Output('padded') personAdded = new EventEmitter<{
     name: string;
     age: number;
   }>();
   @Output('premoved') personRemoved = new EventEmitter();
-  newName = '';
-  newAge = 0;
-
-  @ViewChild('personNameInput', { static: true }) personNameInput: any;
-  constructor() {}
-
-  ngOnInit(): void {}
+  @ViewChild('personNameInput', { static: true }) inputName: any;
+  @ViewChild('personAgeInput', { static: true }) inputAge: any;
 
   addPerson() {
     // console.log(this.personNameInput.nativeElement.value);
     this.personAdded.emit({
-      name: this.personNameInput.nativeElement.value,
-      age: this.newAge,
+      name: this.inputName.nativeElement.value,
+      age: this.inputAge.nativeElement.value,
     });
   }
   removePerson() {
